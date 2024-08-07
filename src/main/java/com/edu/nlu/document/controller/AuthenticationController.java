1e921f11bd306fa7610a8845d70f31dfb547364a
men3sgit <menesforenglish102@gmail.com>
2024-08-07 17:53:50 +0700

    [menes][new] last push to an com toi

--- src/main/java/com/edu/nlu/document/controller/AuthenticationController.java
+++ src/main/java/com/edu/nlu/document/controller/AuthenticationController.java
@@ -15,10 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = "/auth/v1")
 public class AuthenticationController {
 
-    private final AuthenticationService authenticationService;
-    private final DocumentService documentService;
-    private final AuthenticationManager authenticationManager;
-
     @GetMapping(path = "/login")
     public String showLoginForm(Model model) {
         model.addAttribute("loginForm", new LoginForm());
---
+++ src/main/java/com/edu/nlu/document/payload/DocumentForm.java
@@ -0,0 +1,56 @@
+package com.edu.nlu.document.payload;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@ToString
+@Getter
+@Setter
+public class DocumentForm {
+    @NotEmpty(message = "Số văn bản is required")
+    private String documentNumber;
+
+    private boolean isLegalDocument;
+
+    @NotEmpty(message = "Số đến is required")
+    private String arrivalNumber;
+
+    @NotNull(message = "Ngày đến is required")
+    private LocalDateTime arrivalDate;
+
+    @NotEmpty(message = "Số ký hiệu của văn bản is required")
+    private String documentSymbol;
+
+    @NotNull(message = "Ngày ban hành is required")
+    private LocalDate issueDate;
+
+    @NotEmpty(message = "Cơ quan ban hành is required")
+    private String issuingAgency;
+
+    private String agencyLevel;
+
+    private String documentType;
+
+    private String field;
+
+    @NotEmpty(message = "Trích yếu is required")
+    private String summary;
+
+    private String note;
+
+    private boolean isDirectiveDocument;
+
+    private LocalDate processingTime;
+
+    private boolean hasPaperCopy;
+
+    private List<MultipartFile> attachedFiles;
+}
